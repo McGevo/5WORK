09538c908eed0b3e8f7877cb1607cbf16eb2e8bf
Sean Daly <smdaly88@gmail.com>
2020-07-18 20:17:25 +0930

    test

---
+++ EATApp/EATApp/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using EATApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EATApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+
+        public ActionResult LoginView()
+        {
+            ViewBag.Message = "LoginView Page";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorise(EATApp.Models.lecturer lecturerModel)
+        {
+            using (TafeDBEntities db = new TafeDBEntities())
+            {
+                var userDetails = db.lecturers.Where(x => x.EmailAddress == lecturerModel.EmailAddress && x.LecturerID == lecturerModel.LecturerID).FirstOrDefault();
+                if(userDetails==null)
+                {
+                    lecturerModel.LoginErrorMessage = "Email or Password is incorrect";
+                    return View("LoginView", lecturerModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.LecturerID;
+                    return RedirectToAction("ClassView", "Teacher");
+                }
+            }
+
+                
+        }
+    }
+}
---
+++ EATApp/EATApp/Controllers/StudentLoginController.cs
@@ -0,0 +1,39 @@
+﻿using EATApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EATApp.Controllers
+{
+    public class StudentLoginController : Controller
+    {
+        // GET: StudentLogin
+        public ActionResult StudentLoginView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorise(EATApp.Models.student studentModel)
+        {
+            using (TafeDBEntities db = new TafeDBEntities())
+            {
+                var userDetails = db.students.Where(x => x.EmailAddress == studentModel.EmailAddress && x.StudentID == studentModel.StudentID).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    studentModel.LoginErrorMessage = "Email or Password is incorrect";
+                    return View("StudentLoginView", studentModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.StudentID;
+                    return RedirectToAction("StudentView", "Teacher");
+                }
+            }
+
+
+        }
+    }
+}
